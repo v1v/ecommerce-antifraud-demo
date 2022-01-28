d8caba7e0ff96055f3bdd4d8581122d6ca340b78
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-01-28 14:51:22 +0000

    enable RestController

--- src/main/java/com/mycompany/ecommerce/antifraud/AntiFraudApplication.java
+++ src/main/java/com/mycompany/ecommerce/antifraud/AntiFraudApplication.java
@@ -2,12 +2,20 @@ package com.mycompany.ecommerce.antifraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AntiFraudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AntiFraudApplication.class, args);
 	}
 
+	@GetMapping("/ecommerce")
+	public String ecommerce(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
