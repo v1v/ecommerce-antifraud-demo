da4b03c6b09e3bfe80aac1f2343f87f36b903471
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-01-31 10:57:25 +0000

    healthcheck service

--- src/main/java/com/mycompany/ecommerce/antifraud/AntiFraudApplication.java
+++ src/main/java/com/mycompany/ecommerce/antifraud/AntiFraudApplication.java
@@ -2,9 +2,14 @@ package com.mycompany.ecommerce.antifraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
@@ -17,5 +22,10 @@ public class AntiFraudApplication {
 	@GetMapping("/ecommerce")
 	public String ecommerce(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+	}
+
+	@RequestMapping(value = "/healthcheck", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map healthcheck() {
+		return Collections.singletonMap("response", "Status Up");
 	}
 }
