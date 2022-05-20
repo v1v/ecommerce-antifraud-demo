cc0d467eaa96bec2e626d2ed58450eaa292ad258
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-05-20 15:01:08 +0100

    use main

--- post-hook.sh
+++ post-hook.sh
@@ -5,4 +5,4 @@ JENKINS_URL="http://localhost:8080"
 if [ ! -f jenkins-cli.jar ] ; then
 	wget http://localhost:8080/jnlpJars/jenkins-cli.jar
 fi
-java -jar jenkins-cli.jar -s "${JENKINS_URL}/" -webSocket build antifraud/antifraud/demo
+java -jar jenkins-cli.jar -s "${JENKINS_URL}/" -webSocket build antifraud/antifraud/main
